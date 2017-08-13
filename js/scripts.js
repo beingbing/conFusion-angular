e8db6b75265067672a2af2a550c26be4c0b2d36e
Samar <shaikhsamartaj@gmail.com>
2017-08-13 11:41:24 +0530

    starting point

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    $("#mycarousel").carousel({interval: 2000});
+    $("#carousel-button").click(function(){
+
+        if($("#carousel-button").children("span").hasClass('fa-pause'))
+        {
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass('fa-pause');
+            $("#carousel-button").children("span").addClass('fa-play');
+        }
+        else if($("#carousel-button").children("span").hasClass('fa-play'))
+        {
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass('fa-play');
+            $("#carousel-button").children("span").addClass('fa-pause');
+        }
+    }); 
+});
+
+
+$(document).ready(function() {
+
+        $('#reserveTableButton').click(function() {
+            $('#reservemodal').modal({show: true});
+        });
+
+        $('#loginLink').click(function() {
+            $('#loginModal').modal({show: true});
+        });
+});
